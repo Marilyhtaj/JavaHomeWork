1ec657fd0651e8d4043fc8bce802160e2900dd32
Maria Lyhtaj <Lyhtaj.manjasha@gmail.com>
2021-11-24 17:28:15 +0200

    init commit

--- src/com/pb/lyhtaj/hw9/FileNumbers.java
+++ src/com/pb/lyhtaj/hw9/FileNumbers.java
@@ -1,31 +1,13 @@
 package com.pb.lyhtaj.hw9;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class FileNumbers {
 
     public static void main(String[] args) throws Exception{
-     //   Path path = Paths.get("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt");
-
         createNumbersFile();
-//        try (Scanner scan = new Scanner(path)){
-//            System.out.println("Начинаем чтение файла...");
-//            while (true){
-//                String str = scan.nextLine();
-//                System.out.println(str);
-//            }
-//        }catch (NoSuchElementException ex){
-//            System.out.println("Строки для прочтения законились");
-//            System.out.println( "--------------------------------");
-//        } catch (IOException ex){
-//            ex.printStackTrace();
-//        }
-
         createOddNumbersFile();
     }
 
@@ -50,29 +32,52 @@ public class FileNumbers {
     }
 
 
+//    public static void  createOddNumbersFile() throws Exception{
+//        System.out.println("Читаем файл \"" + Paths.get("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt") + "\":");
+//        try (BufferedReader reader = Files.newBufferedReader(Paths.get("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt"))){
+//            String line;
+//            while ((line = reader.readLine()) !=null){
+//                Writer writer1 = new FileWriter("C:\\Users\\Мария\\Desktop\\Java\\odd-numbers.txt");
+//                int [] mas = new int[100];
+//                for(int i=0;i< mas.length;i++) {
+//                    if(i%2 == 1) {
+//                        mas[i] = 0;
+//                        writer1.write(line);
+//                    }
+//                }
+//                //writer1.write(line);
+//                writer1.close();
+//            }
+//            System.out.println();
+//
+//    }catch (Exception ex){
+//            System.out.println("Ошибка чтения файла: " + ex);
+//        }
+//        System.out.println("Создан и записан новый файл");
+//        }
+
     public static void  createOddNumbersFile() throws Exception{
         System.out.println("Читаем файл \"" + Paths.get("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt") + "\":");
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt"))){
-            String line;
-            while ((line = reader.readLine()) !=null){
-                Writer writer1 = new FileWriter("C:\\Users\\Мария\\Desktop\\Java\\odd-numbers.txt");
-                int [] mas = new int[100];
-                for(int i=0;i< mas.length;i++) {
-                    if(i%2 == 1) {
-                        mas[i] = 0;
-                        writer1.write(line);
-                    }
+        try (Scanner reader = new Scanner (new File("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt"));
+        Writer writer1 = new FileWriter("C:\\Users\\Мария\\Desktop\\Java\\odd-numbers.txt");) {
+            while (reader.hasNextLine()) {
+                Scanner line = new Scanner(reader.nextLine());
+                while (line.hasNextInt()) {
+                    int dat = line.nextInt();
+                    if (dat % 2 == 1) {
+                        System.out.format("%d -> 0, " + dat);
+                        dat = 0;
+                    } else System.out.print(dat + ", ");
+                    writer1.write(dat + ", ");
                 }
-                //writer1.write(line);
-                writer1.close();
+                line.close();
+                // writer1.();
             }
-            System.out.println();
-
-    }catch (Exception ex){
+        }catch (Exception ex){
             System.out.println("Ошибка чтения файла: " + ex);
         }
         System.out.println("Создан и записан новый файл");
+    }
 
 
-        }
     }
