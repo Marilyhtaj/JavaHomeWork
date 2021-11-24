c25f09fbc1ad521e449b17ffb552b17b06d8b783
Maria Lyhtaj <Lyhtaj.manjasha@gmail.com>
2021-11-24 15:08:29 +0200

    init commit

--- src/com/pb/lyhtaj/hw9/FileNumbers.java
+++ src/com/pb/lyhtaj/hw9/FileNumbers.java
@@ -1,8 +1,7 @@
 package com.pb.lyhtaj.hw9;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.NoSuchElementException;
@@ -10,40 +9,60 @@ import java.util.Scanner;
 
 public class FileNumbers {
 
-    public static void createNumbersFile() throws Exception{
-        try {Writer writer = new FileWriter("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt");
-            int [][] array = new int[10][10];
-             for(int i=0;i< array.length;i++, System.out.println()){
-                 for(int j=0;j< array.length;j++){
-                     array[i][j] = ((int) (Math.random()*99+1));
-                     System.out.println(array[i][j] + " ");
-                     writer.write(array[i][j] + " ");
-                 }
-             }writer.write("\n");
-            writer.close();
-        }catch (Exception ex){
-            System.out.println("Ошибка: " + ex);
-        }
-    }
-
-//    public void  createOddNumbersFile(){
-//
-//
-//    }
-
     public static void main(String[] args) throws Exception{
         Path path = Paths.get("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt");
-             createNumbersFile();
 
+        createNumbersFile();
         try (Scanner scan = new Scanner(path)){
+            System.out.println("Начинаем чтение файла...");
             while (true){
                 String str = scan.nextLine();
                 System.out.println(str);
             }
         }catch (NoSuchElementException ex){
             System.out.println("Строки для прочтения законились");
+            System.out.println( "--------------------------------");
         } catch (IOException ex){
             ex.printStackTrace();
         }
+
+        createOddNumbersFile();
     }
+
+    public static void createNumbersFile() throws Exception{
+        try {Writer writer = new FileWriter("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt");
+            int size = 10;
+            int [][] array = new int[size][size];
+             for(int i=0;i< size;i++){
+                 for(int j=0;j< size;j++){
+                     array[i][j] = ((int) (Math.random() * 99) + 1);
+                    System.out.print(array[i][j] + " ");
+                     writer.write(array[i][j] + "\n");
+                     }
+                  System.out.println();
+                 }
+            writer.close();
+        }catch (Exception ex){
+            System.out.println("Ошибка: " + ex);
+        }
+        System.out.println("Файл создан и наполнен ");
+        System.out.println( "---------------------------------");
+    }
+
+
+    public static void  createOddNumbersFile() throws Exception{
+        System.out.println("Читаем файл \"" + Paths.get("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt") + "\":");
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("C:\\Users\\Мария\\Desktop\\Java\\numbers.txt"))){
+            String line;
+            while ((line = reader.readLine()) !=null){
+                System.out.println(line);
+            }
+            System.out.println();
+    }catch (Exception ex){
+            System.out.println("Ошибка чтения файла: " + ex);
+        }
+        System.out.println("Чтение завершено");
+    }
+
+
 }
